25c15c4047214e7f0fa3f4721c3b41824df57612
Jakalanern <77250250+Jakalanern@users.noreply.github.com>
2021-01-10 17:25:59 -0800

    Create switchstatements.cs

---
+++ switchstatements.cs
@@ -0,0 +1,49 @@
+using System;
+
+namespace Switch_Statements__Day_of_the_week_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {      
+            Console.WriteLine(GetDay(6));
+
+            Console.ReadLine();
+        }
+
+        static string GetDay(int dayNum)
+        {
+            string dayName;
+
+            switch(dayNum)
+            {
+                case 0:
+                    dayName = "Sunday";
+                    break;
+                case 1:
+                    dayName = "Monday";
+                    break;
+                case 2:
+                    dayName = "Tuesday";
+                    break;
+                case 3:
+                    dayName = "Wednesday";
+                    break;
+                case 4:
+                    dayName = "Thursday";
+                    break;
+                case 5:
+                    dayName = "Friday";
+                    break;
+                case 6:
+                    dayName = "Saturday";
+                    break;
+                default:
+                    dayName = "Invalid Day Number";
+                    break;
+            }
+
+            return dayName;
+        }
+    }
+}
